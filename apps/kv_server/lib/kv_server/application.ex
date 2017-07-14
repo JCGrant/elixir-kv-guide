23290a79046c80a27380a634631b4922e08aa299
JCGrant <jamescolin.grant@gmail.com>
2017-07-14 19:21:42 +0100

    Run KVServer.accept(4040) as a worker

--- apps/kv_server/lib/kv_server/application.ex
+++ apps/kv_server/lib/kv_server/application.ex
@@ -12,6 +12,7 @@ defmodule KVServer.Application do
     children = [
       # Starts a worker by calling: KVServer.Worker.start_link(arg1, arg2, arg3)
       # worker(KVServer.Worker, [arg1, arg2, arg3]),
+      worker(Task, [KVServer, :accept, [4040]])
     ]
 
     # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
